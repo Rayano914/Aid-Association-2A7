7bfe7ab0af33257c2fa6e842ee4443df934dc594
nada176 <nada.kort0@gmail.com>
2022-03-03 16:40:45 +0100

    crud

---
+++ caisse.cpp
@@ -0,0 +1,104 @@
+#include "caisse.h"
+#include <QSqlQuery>
+#include <QtDebug>
+#include <QObject>
+//constructeur par defaut:
+ caisse::caisse ()
+{
+    id_operation=0;
+    date_operation=0;
+    type_operation="";
+    montant=0;
+
+}
+ //constructeur parametre:
+caisse::caisse(int id_operation, int date_operation ,  QString type_operation, int montant)
+{
+    this->id_operation=id_operation;
+    this->date_operation=date_operation;
+    this->type_operation=type_operation;
+    this->montant=montant;
+
+
+
+}
+
+int caisse::getid_operation(){return id_operation;}
+int caisse::getdate_operation(){return date_operation;}
+QString caisse::gettype_operation(){return type_operation;}
+int caisse::getmontant(){return montant;}
+
+void caisse::setid_operation(int id_operation){this->id_operation=id_operation;}
+void caisse::setdate_operation(int date_operation){this->date_operation=date_operation;}
+void caisse::settype_operation(QString type_operation){this->type_operation=type_operation;}
+
+void caisse::setmontant(int montant){this->montant=montant;}
+
+
+
+
+//--------------------------------------crud--------------------------------------------------------------
+//ajouter:
+
+bool caisse::ajouter()
+{
+
+
+QSqlQuery query;
+query.prepare("insert into caisse (id_operation,date_operation,type_operation,montant)"
+              "values (:id_operation,:date_operation,:type_operation,:montant)");
+
+query.bindValue(":id_operation", id_operation);
+query.bindValue(":date_operation", date_operation );
+query.bindValue(":type_operation", type_operation );
+query.bindValue(":montant", montant );
+
+
+
+
+
+return  query.exec();
+
+}
+//suprrimer:
+bool caisse::supprimer(int id_operation)
+                                                                          {
+    QSqlQuery query;
+    query.prepare("Delete from caisse where id_operation=:id_operation");
+
+    query.bindValue(":id_operation", id_operation);
+
+    return query.exec();
+
+
+
+}
+//afficher:
+QSqlQueryModel* caisse::afficher()
+{
+QSqlQueryModel * model=new QSqlQueryModel();
+
+model->setQuery("SELECT * FROM caisse ");
+
+
+
+return model;
+}
+//modifier:
+bool caisse::modifier(){
+    QSqlQuery query;
+    QString id_string = QString::number(id_operation);
+
+    query.prepare("update caisse set id_operation=:id_operation,date_operation=:date_operation,type_operation=:type_operation,montant=:montant where id_operation=:id_operation");
+
+    query.bindValue(":id_operation", id_operation);
+    query.bindValue(":date_operation", date_operation );
+
+    query.bindValue(":type_operation", type_operation );
+    query.bindValue(":montant", montant );
+
+
+
+
+    return (query.exec());
+}
---
+++ mainwindow.cpp
@@ -0,0 +1,130 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "caisse.h"
+#include "QMessageBox"
+#include <QDebug>
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    ui->stackedWidget->setCurrentIndex(1);;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_fleche_clicked()
+{
+  ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_ajouter_caisses_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+void MainWindow::on_afficher_caisses_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+     ui->tablecaisse->setModel(c.afficher());
+}
+
+
+void MainWindow::on_modifier_caisses_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(3);
+}
+
+
+void MainWindow::on_supprimer_caisses_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(4);
+}
+
+
+void MainWindow::on_ajoute_caisse_clicked()
+{
+    //recuperation des informations saises:
+    int id_operation=ui->leidoperation->text().toInt();
+    int date_operation=ui->ledateoperation->text().toInt();
+      QString type_operation=ui->letypeoperation->text();
+      int montant=ui->lemontant->text().toInt();
+      caisse c (id_operation,date_operation,type_operation,montant);
+      bool test=c.ajouter();
+      QMessageBox msgBox;
+
+        if(test)
+          {  msgBox.setText("Ajout avec succes.");
+            ui->tablecaisse->setModel(c.afficher());
+        }
+        else
+           { msgBox.setText("Echec d'ajout");}
+            msgBox.exec();
+
+
+}
+
+void MainWindow::on_supprimer_caisse_clicked()
+{
+    caisse c; c.setid_operation(ui->leidoperation_2->text().toInt());
+        bool test=c.supprimer(c.getid_operation());
+        QMessageBox msgBox;
+
+        if(test)
+           { msgBox.setText("Suppression avec succes.");
+        ui->tablecaisse->setModel(c.afficher());
+
+        }
+        else
+           { msgBox.setText("Echec de suppression");}
+            msgBox.exec();
+}
+
+
+
+
+
+void MainWindow::on_modifier_caisse_clicked()
+{
+    {
+        bool test=false;
+
+
+            int id_operation=ui->lineEdit_modif_id->text().toInt();
+            int date_operation=ui->lineEdit_modif_date->text().toInt();
+            QString type_operation=ui->lineEdit_modif_type->text();
+            int montant=ui->lineEdit_modif_montant->text().toInt();
+
+
+            //Appel du constructeur parametré
+            caisse c(id_operation,date_operation,type_operation,montant);
+
+
+                test=c.modifier();
+                if(test)//si requete executer ==>QMessageBox::information
+                {
+                    ui->lineEdit_modif_id->clear();
+                    ui->lineEdit_modif_date->clear();
+                    ui->lineEdit_modif_type->clear();
+                    ui->lineEdit_modif_montant->clear();
+
+
+                    //ui->tableView->setModel(c.afficher());
+                    QMessageBox::information(nullptr, QObject::tr("OK"),
+                             QObject::tr("Modification effectué\n""Click Cancel to exit."),QMessageBox::Cancel);
+
+                }
+                else
+                {
+                    QMessageBox::critical(nullptr, QObject::tr("OK"),
+                             QObject::tr("Modification non effectué\n""Click Cancel to exit."),QMessageBox::Cancel);
+                }
+    }
+}
